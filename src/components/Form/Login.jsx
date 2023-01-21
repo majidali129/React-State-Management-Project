c83dab0e668a2e9996423672299e05c82b3da785
Majid Ali <lahmajidali@gmail.com>
2023-01-21 12:11:06 -0800

    done with useReducer

--- src/components/Form/Login.jsx
+++ src/components/Form/Login.jsx
@@ -1,16 +1,30 @@
 
 
-import React , {useState , useEffect} from 'react'
+import React , {useState , useEffect , useReducer } from 'react'
 import Card from '../UI/Card/Card'
 import classes from "./Login.module.css"
 import Button from "../UI/Button/Button"
+
+const EmailReducer = (currentState , actions )=>{
+  if(actions.type === "userEmail"){
+    return ({value : actions.value , isValid : actions.value.includes("@")})
+  }
+  return({ value : "" , isValid : false})
+}
+
+const PasswordReducer = (currentState , actions )=>{
+  if(actions.type === "userPassword"){
+    return ({value : actions.value , isValid : actions.value.trim().length > 4})
+  }
+  return({ value : "" , isValid : false})
+}
 
 const Login = (props)=>{
 
   // const [firstName , setFirstName ] = useState("");
   // const [lastName , setLastName ] = useState("");
-  const [email , setEmail ] = useState("");
-  const [password , setPassword ] = useState("");
+  // const [email , setEmail ] = useState("");
+  // const [password , setPassword ] = useState("");
   const [validEmail , setValidEmail] = useState()
   const [validPassword , setValidPassword] = useState()
   const [formIsValid , setFormIsValid] = useState(false)
@@ -22,35 +36,54 @@ const Login = (props)=>{
   //   setLastName(event.target.value)
   // }
 
+
+  const [userEmail , emailDispatcher] = useReducer(EmailReducer , {
+    value : "",
+    isValid : null
+  })
+
+  const [userPassword , passwordDispatcher] = useReducer(PasswordReducer , {
+    value : "",
+    isValid : null
+  })
+
+
+  const {isValid : emailIsValid} = userEmail
+  const {isValid : passwordIsValid} = userPassword
+
   useEffect(()=>{
     const validityIdentifier = setTimeout(() => {
       setFormIsValid(
-        email.includes("@") && password.trim().length > 4
+        emailIsValid && passwordIsValid
       )
     }, 500);
     return ()=>{
       clearTimeout(validityIdentifier)
     }
-  },[email , password])
+  },[emailIsValid , passwordIsValid])
+
   const EmailChangeHandler = (event)=>{
-    setEmail(event.target.value)
+    // setEmail(event.target.value)
     // setFormIsValid(event.target.value.includes("@") && password.trim().length > 4)
+    emailDispatcher({type:"userEmail" , value : event.target.value})
   }
   const PasswordChangeHandler = (event)=>{
-    setPassword(event.target.value);
+    // setPassword(event.target.value);
     // setFormIsValid(email.includes("@") && event.target.value.trim().length > 4)
+    passwordDispatcher({type : "userPassword" , value : event.target.value})
+
   }
 
   const EmailValidityHandler = ()=>{
-    setValidEmail(email.includes('@'));
+    setValidEmail(userEmail.value.includes('@'));
   }
   const PasswordValidityHandler = ()=>{
-    setValidPassword(password.trim().length > 4);
+    setValidPassword(userPassword.value.trim().length > 4);
   }
   
   const LoginHandler = (event)=>{
     event.preventDefault()
-    props.userData(email , password)
+    props.userData(userEmail.value , userPassword.value)
     // console.log(firstName , lastName , email , password)
 
   }
@@ -80,10 +113,10 @@ const Login = (props)=>{
           />
         </div> */}
 
-        <div className={`${classes.control} ${validEmail === false ? classes.inValid : ""}`}>
+        <div className={`${classes.control} ${userEmail.isValid === false ? classes.inValid : ""}`}>
            <label htmlFor="email">Email :</label>      
           <input 
-          value={email}
+          value={userEmail.value}
           type="text" 
           id='email' 
           onChange={EmailChangeHandler}
@@ -91,10 +124,10 @@ const Login = (props)=>{
           />
         </div>
 
-        <div className={`${classes.control} ${validPassword === false ? classes.inValid : ""}`}>
+        <div className={`${classes.control} ${userPassword.isValid === false ? classes.inValid : ""}`}>
            <label htmlFor="password">Password :</label>      
           <input 
-          value={password}
+          value={userPassword.value}
           type="text" 
           id='password' 
           onChange={PasswordChangeHandler} 
